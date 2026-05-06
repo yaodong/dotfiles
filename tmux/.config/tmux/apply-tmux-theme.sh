fde34e474326e3d3e3413c5a8c31098cd7cce5a6
Yaodong Zhao <yaodong.dev@gmail.com>
2026-05-05 18:01:53 -0700

    Fix tmux theme toggle window labels

--- tmux/.config/tmux/apply-tmux-theme.sh
+++ tmux/.config/tmux/apply-tmux-theme.sh
@@ -23,6 +23,13 @@ tmux show -g 2>/dev/null | awk '/^@(thm_|catppuccin_|_ctp_)/ { print "set -gu " 
 {
   printf "set -g @catppuccin_flavor '%s'\n" "$flavor"
   printf "%s\n" 'set -g @catppuccin_status_background "none"'
+  printf "%s\n" 'set -g @catppuccin_window_status_style "basic"'
+  printf "%s\n" 'set -g @catppuccin_window_current_left_separator " "'
+  printf "%s\n" 'set -g @catppuccin_window_current_middle_separator " "'
+  printf "%s\n" 'set -g @catppuccin_window_current_right_separator " "'
+  printf "%s\n" 'set -g @catppuccin_window_text " #W"'
+  printf "%s\n" 'set -g @catppuccin_window_current_text " #W"'
+  printf "%s\n" 'set -g @catppuccin_date_time_text " %m/%d %H:%M"'
   printf "%s\n" "run-shell '~/.config/tmux/plugins/tmux/catppuccin.tmux'"
   printf "%s\n" 'set -gF @_status_battery "#{E:@catppuccin_status_battery}"'
   printf "%s\n" "run-shell '~/.config/tmux/plugins/tmux-battery/battery.tmux'"
